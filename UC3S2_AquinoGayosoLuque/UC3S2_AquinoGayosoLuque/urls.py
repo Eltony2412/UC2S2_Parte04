d35fba622b169516eaa1fcfaa53332ed0b02bc22
unknown <1913050729@untel.edu.pe>
2022-12-07 10:43:54 -0500

    UC3S2_Parte02_Gayoso

--- UC3S2_AquinoGayosoLuque/UC3S2_AquinoGayosoLuque/urls.py
+++ UC3S2_AquinoGayosoLuque/UC3S2_AquinoGayosoLuque/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from miapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('inicio/', views.inicio, name = "inicio"),
+    path('', views.index, name = "index"),
 ]
--- UC3S2_AquinoGayosoLuque/miapp/views.py
+++ UC3S2_AquinoGayosoLuque/miapp/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+ 
+menu = """
+    <h1>MENU</h1>
+        <br>
+        <ul>
+            <li>
+                <a href = "/inicio">INICIO</a>
+            </li>
+        </ul>
+"""
 
-# Create your views here.
+def index (request):
+    MENU ="""
+          """
+    return HttpResponse(menu + MENU)
+
+
+def inicio(request):
+    mensaje="""
+                <H1>LP3</H1>
+                <br>
+                <H2>Integrantes:</H2>
+                <ul>
+                    <li> Carlos</li>
+                    <li> Elberth</li>
+                    <li> Luis</li>
+                </ul>
+            """
+    return HttpResponse(menu + mensaje)
